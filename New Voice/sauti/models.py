5d706d073319c02661369bb9cf5d88600d43801c
nimrodouko <oukonimrod@gmail.com>
2023-04-20 23:55:53 +0300

    database

--- New Voice/sauti/models.py
+++ New Voice/sauti/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Written(models.Model):
-    transcript = models.TextField()
-    audio_file = models.FileField(null=True)
-    word_timings = models.TextField(null =True)
+    text = models.TextField(max_length=500)
+
+    
+  
 
     
--- New Voice/sauti/speech_to_text.py
+++ New Voice/sauti/speech_to_text.py
@@ -1,13 +1,16 @@
 import pyaudio
 import speech_recognition as sr
+from .models import Written
 def  recognize_speech():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("speak now:")
-        audio = r.listen(source, timeout = 10)
+        audio = r.listen(source, timeout = 5)
     
     try:
         text = r.recognize_google(audio)
+        written =Written(text=text)
+        written.save()
         return(text)
     except sr.UnknownValueError:
         print("Could not understand audio")
